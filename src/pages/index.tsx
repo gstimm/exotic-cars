ed62f45ba017efa2a1c367f582b9878a0789120f
gstimm <contato.gabrieltimm@gmail.com>
2021-09-16 10:31:13 -0300

    feat: add back to top button in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,99 @@
-import { Container, Content } from '../styles/home.styles';
+import { Container, Content, Grid } from '../styles/home.styles';
+import { useEffect, useState } from 'react';
 
 import API from '../utils/cars.json';
 import CarCard from '../components/CarCard';
+import { GetStaticProps } from 'next';
 import Header from '../components/Header';
+import Image from 'next/image';
 import Link from 'next/link';
 
-export default function Home() {
+type Options = {
+  option_id: number;
+  color: string;
+  image_url: string;
+};
+
+type Car = {
+  id: number;
+  brand: string;
+  model: string;
+  price: number;
+  main_image_url: string;
+  brand_logo_url: string;
+  options: Options[];
+};
+
+interface CarsProps {
+  cars: Car[];
+}
+
+export default function Home({ cars }: CarsProps) {
+  const [showScroll, setShowScroll] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', checkScrollTop);
+    return function cleanup() {
+      window.removeEventListener('scroll', checkScrollTop);
+    };
+  });
+
+  const checkScrollTop = () => {
+    if (!showScroll && window.scrollY > 100) {
+      setShowScroll(true);
+    } else if (showScroll && window.scrollY <= 100) {
+      setShowScroll(false);
+    }
+  };
+
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
     <>
       <Header />
       <Container>
         <Content>
-          {API.cars.map(car => (
-            <Link key={car.id} href={`/car/${car.id}`} passHref>
-              <a>
-                <CarCard
-                  brand={car.brand}
-                  model={car.model}
-                  price={car.price}
-                  main_image_url={car.main_image_url}
+          <Grid>
+            {cars.map(car => (
+              <Link key={car.id} href={`/car/${car.id}`} passHref>
+                <a>
+                  <CarCard
+                    brand={car.brand}
+                    model={car.model}
+                    price={car.price}
+                    main_image_url={car.main_image_url}
+                  />
+                </a>
+              </Link>
+            ))}
+          </Grid>
+          <div className='button-div'>
+            {showScroll && (
+              <button onClick={scrollTop}>
+                <Image
+                  src='/assets/back_to_top.svg'
+                  alt='Back to Top Button'
+                  width={100}
+                  height={100}
                 />
-              </a>
-            </Link>
-          ))}
+              </button>
+            )}
+          </div>
         </Content>
       </Container>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const cars = API.cars;
+
+  return {
+    props: {
+      cars,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
--- src/styles/home.styles.ts
+++ src/styles/home.styles.ts
@@ -2,19 +2,32 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   min-height: calc(100vh - 5.1875rem);
-
   padding-top: 22px;
+`;
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  column-gap: 24px;
+  row-gap: 32px;
 `;
 
 export const Content = styled.div`
   max-width: 1220px;
   margin: 0 auto;
-
-  display: grid;
-
-  grid-template-columns: repeat(4, 1fr);
-  column-gap: 24px;
-  row-gap: 32px;
-
   margin-bottom: 57px;
+
+  .button-div {
+    width: 100px;
+    position: sticky;
+    margin-left: auto;
+    bottom: 0px;
+
+    button {
+      background: transparent;
+      border: none;
+      width: 100px;
+      height: 100px;
+    }
+  }
 `;
